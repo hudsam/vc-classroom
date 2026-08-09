bb34e39e364cf6aa23fa2ba32fe7584bbde0c795
hudsam <app.hoedhya@gmail.com>
2026-08-09 19:04:45 +0700

    changed sslmode

--- server/config/db.js
+++ server/config/db.js
@@ -9,9 +9,7 @@ if (!process.env.DATABASE_URL) {
 
 export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: process.env.DATABASE_URL?.includes('localhost') || process.env.DATABASE_URL?.includes('127.0.0.1')
-    ? undefined
-    : { rejectUnauthorized: false },
+  ssl: process.env.DATABASE_URL?.includes('sslmode=require') ? { rejectUnauthorized: false } : undefined,
 });
 
 export function query(text, params) {
